007b9fd3874b24d4d1072f017d7e2482651682be
SDodson <siddodson@gmail.com>
2016-07-15 18:39:18 -1000

    Product Router Completed

--- routes/productsRoutes.js
+++ routes/productsRoutes.js
@@ -1,34 +1,45 @@
 module.exports = (function(){
 
-var productArray = [];
-var uniqueID = 0;
+  var productArray = [];
+  var uniqueID = 0;
 
-function post(req, res){
-  req.body.id = uniqueID++;
-  productArray.push(req.body);
-  res.send({'sucess': true});
-}
-
-function put(req, res){
-  for ( var i = 0; i < productArray.length; i++){
-    if(parseFloat(req.body.id)=== productArray[i].id)
-      for (var key in req.body){
-        (productArray[i][key] = req.body[key]);
-    }
-    return res.send({'sucess': true});
+  function post(req, res){
+    req.body.id = uniqueID++;
+    productArray.push(req.body);
+    res.send({'sucess': true});
   }
+
+  function put(req, res){
+    for ( var i = 0; i < productArray.length; i++){
+      if(parseFloat(req.body.id) === productArray[i].id){
+        for (var key in req.body){
+          (productArray[i][key] = req.body[key]);
+        }
+        res.status(200).send({'sucess': true});
+        return;
+      }
+    }
     return res.send({'sucess': false});
-}
+  }
 
 
-function delete(req, res){
+  function deleter(req, res){
+    for ( var i = 0; i < productArray.length; i++){
+      if(parseFloat(req.body.id) === body.param.id){
+        productArray.splice(i,1);
+        res.status(200).send({'sucess': true});
+        return;
+      }
+    return res.send({'sucess': false});
+    }
+  }
 
-};
 
-
-function itemById(req, res){
-
-}
-
+  function itemById(req, res){
+    if(parseFloat(productArray[i].id) === body.param.id){
+      return;
+    }
+  }
 
 })();
+
